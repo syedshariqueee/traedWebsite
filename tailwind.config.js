475097a134e076b934737a47189a0e87ec3afe74
syedshariqueee <syedshariqueee@gmail.com>
2024-03-16 06:02:23 +0530

    final

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        'frame1':'rgba(12, 32, 39, 1)',
+        'rgba1' : 'rgba(255, 255, 255, 1)',
+        'div1' : 'rgba(126, 134, 147, 0.1)',
+        'ios' : 'rgba(0, 0, 0, 0.3)',
+        'body' : 'rgba(35, 33, 59, 1)',
+      }
+    },
+  },
+  plugins: [],
+}
+
